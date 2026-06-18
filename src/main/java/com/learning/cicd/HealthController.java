25538455a7697f7119a4d4d423ad06d527bfb0af
gowrisankar-encipher <gowrisankar.v@encipherhealth.com>
2026-06-18 16:29:22 +0530

    initial commit

---
+++ src/main/java/com/learning/cicd/HealthController.java
@@ -0,0 +1,15 @@
+package com.learning.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/check")
+    public String checkHealth() {
+        return "Application is running and Health check is ok";
+    }
+}
